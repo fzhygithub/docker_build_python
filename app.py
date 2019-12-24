c5324e86a2ce37002aa23a22368ed1c069bcb5f6
fzhygithub <6562157@qq.com>
2019-12-24 12:06:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-#from flask import Flask
-#from redis import Redis, RedisError
+###from flask import Flask
+###from redis import Redis, RedisError
 import os
 import socket
 
 # Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+###redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
